7999ce18a7681a47efbb2a874578eb1d7560e88d
gars150387 <gar.santeliz@gmail.com>
2021-06-08 01:01:51 +0000

    origin

---
+++ script.js
@@ -0,0 +1,22 @@
+function randomText(){
+let who = ['The dog','My grandma','His turtle','My bird'];
+let action = ['ate','peed','crushed','broke'];
+let what = ['my homework', 'the keys', 'the car'];
+let when = ['before the class','right on time','when I finished','during my lunch','while I was praying'];
+
+let whoIndex =Math.floor((Math.random())* who.length);
+let actionIndex =Math.floor((Math.random())* action.length);
+let whatIndex =Math.floor((Math.random())* what.length);
+let whenIndex =Math.floor((Math.random())* when.length);
+// console.log(who[whoIndex])
+// console.log(action[actionIndex])
+// console.log(what[whatIndex])
+// console.log(when[whenIndex])
+return who[whoIndex] +' '+ action[actionIndex]+' '+ what[whatIndex]+' '+ when[whenIndex];
+
+}
+let randomExcuse = randomText()
+console.log(randomExcuse)
+
+// function myFunction() {
+//   document.getElementById("demo").innerHTML = randomText;
